c5af2e82991d44f116baec71ee0769cbc084de32
David <andresdbuitrago@gmail.com>
2024-09-14 07:35:09 -0500

    guardar

--- tiposdedatosimples.py
+++ tiposdedatosimples.py
@@ -10,4 +10,15 @@ nombre = input("Introduce nombre:")
 print('Hola '+nombre)
 """
 
-print(((3+2)/(2*5))**2)
+#print(((3+2)/(2*5))**2)
+
+""""
+n=int(input('Introdusca entero:'))
+print((n*(n+1))/2)
+"""
+
+peso = input('Introduce peso: ')
+estatura = input('Introduce la estatura: ')
+imc = round(float(peso)/float(estatura)**2,2)
+print("Tu imc es: "+str(imc))
+
